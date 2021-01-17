599549a61ab28775a3efdc5f2cdb4c0bc809f44c
vanessapyaar <52893957+vanessapyaar@users.noreply.github.com>
2021-01-16 23:11:54 -0500

    package json updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    pathPrefix: "/my-gatsby-portfolio",
   },
   plugins: [
     {
